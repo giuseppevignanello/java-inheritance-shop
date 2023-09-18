536e0b90846edbb72ee5a6e17299cb91fba209e8
giuseppevignanello <peppe.vignanello@gmail.com>
2023-09-18 14:40:00 +0200

    add getter and setter

--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -2,4 +2,55 @@ package org.lessons.java.shop;
 
 public class Product {
 
+	private String code; 
+	private String name; 
+	private String brand; 
+	private Double price; 
+	private int iva;
+	
+	public Product(String code, String name, String brand, Double price, int iva ) {
+		setName(name);
+		setBrand(brand);
+		setPrice(price); 
+		setIva(iva);
+	}
+	
+	
+	//setters
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public void setPrice(Double price) {
+		this.price= price;
+	}
+	public void setIva(int iva) {
+		this.iva = iva;
+	}
+	
+	//getters 
+	
+	public String getCode() {
+		return code;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getBrand() {
+		return brand;
+	}
+	
+	public Double getPriceBase() {
+		return price;
+	}
+	
+	public int getIva() {
+		return iva;
+	}
+	
 }
